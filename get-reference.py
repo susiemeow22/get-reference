92546199bcce012e560724a0f5290dff625f6482
susiemeow22 <5j869rny24@privaterelay.appleid.com>
2026-05-23 10:50:21 +0800

    Update get-reference.py

--- get-reference.py
+++ get-reference.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 
 # --- 1. 配置信息 ---
 Entrez.email = "susiemeow22@outlook.com" 
-GEMINI_API_KEY = "AIzaSyDY7CQwqjvByzqV5jbMykHF-MN7S8-ZH-4"
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
